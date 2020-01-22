f6c03b7dec57ca2595da7d3679197eb986b4d5e4
mojtaba Shayegh <Shayegh@gmail.com>
2020-01-22 12:46:10 +0330

    maps in seprate components

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import { doArchiveStory } from "../actions/archive";
+
 import { getReadableStories } from "../selectors/story";
 import "./Stories.css";
 import Story from "./Story";
@@ -28,7 +28,7 @@ const COLUMNS = {
   }
 };
 
-const Stories = ({ stories, onArchive }) => (
+const Stories = ({ stories}) => (
   <div className="stories">
     <StoriesHeader columns={COLUMNS} />
     {(stories || []).map(story => (
@@ -36,7 +36,7 @@ const Stories = ({ stories, onArchive }) => (
         key={story.objectID}
         story={story}
         columns={COLUMNS}
-        onArchive={onArchive}
+       
       />
     ))}
   </div>
@@ -56,8 +56,6 @@ const mapStateToProps = state => ({
   stories: getReadableStories(state)
 });
 
-const mapDispatchToProps = dispatch => ({
-  onArchive: id => dispatch(doArchiveStory(id))
-});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Stories);
+
+export default connect(mapStateToProps)(Stories);
--- src/components/Story.js
+++ src/components/Story.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Story.css";
+import {connect} from "react-redux"
+import { doArchiveStory } from "../actions/archive";
 
 const Story = ({ story, columns, onArchive }) => {
   const { title, url, author, num_comments, points, objectID } = story;
@@ -24,4 +26,8 @@ const ButtonInline = ({ onClick,className, type = "button", children }) => (
   </button>
 );
 
-export default Story;
+const mapDispatchToProps = dispatch => ({
+  onArchive: id => dispatch(doArchiveStory(id))
+});
+
+export default connect(null,mapDispatchToProps)(Story);
